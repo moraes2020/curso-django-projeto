e11d0b3857b82138391c3bf9c18c45715cebbfc0
Moraes2020 <mecanicosdemoto@gmail.com>
2022-06-17 14:15:13 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Itailson'
     })
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse("Sobre")
